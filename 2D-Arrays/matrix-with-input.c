bfd3a18373b691dd263786ce3c2ceb4804138735
saeed99-dev <saeedhabib1999@gmail.com>
2025-12-18 23:04:07 +0530

    practice

---
+++ 2D-Arrays/matrix-with-input.c
@@ -0,0 +1,28 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int number[2][2];
+
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             scanf("%d", &number[i][j]);
+//         }
+//         printf("\n");
+
+//     }
+
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             printf("%d\t", number[i][j]);
+//         }
+//         printf("\n");
+
+//     }
+
+//     return 0;
+// }
