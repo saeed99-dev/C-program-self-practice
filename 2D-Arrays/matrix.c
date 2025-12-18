5a6927fff1b574e2a987d48452e0330fa7d00376
saeed99-dev <saeedhabib1999@gmail.com>
2025-12-18 23:05:00 +0530

    Practice

---
+++ 2D-Arrays/matrix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number[4][6];
+    int count = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            // count++;
+            // printf("%d\t", count);
+            printf("saeed\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
